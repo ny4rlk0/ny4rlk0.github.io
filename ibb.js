a26843b60bbf8ef2d727f4854dadd5a5ddbe0cc3
ny4rlk0 <55476474+ny4rlk0@users.noreply.github.com>
2021-08-25 19:02:10 +0300

    Update ibb.js

--- ibb.js
+++ ibb.js
@@ -85,8 +85,8 @@ jQuery(document).ready(function(){
   });
   if (TestModu=true){alert(bilgilendirme_mesaji);}
 });
+anlik_konumu_bul();
 async function konumuBul(){
-          await anlik_konumu_bul();
           var yeni_isaretli_hedef=new google.maps.Marker({position:anlik_konum_obje,map:harita,icon:'kirmizi_ok_x32.png',title:'Konum'});
           if(bilgipenceresi){
             bilgipenceresi.setMap(nyaa_desu);
@@ -109,10 +109,6 @@ async function konumuBul(){
           console.log('KonumuBul()');
           harita.panTo(await anlik_konum_obje);//Haritada konumu ortala
 }
-async function konumual(){
-    await konumuBul();
-} 
-konumual();
 async function konumuAcKapa(){
   async function rotalariTemizle(){
     console.log('Rotalar temizleniyor...');
